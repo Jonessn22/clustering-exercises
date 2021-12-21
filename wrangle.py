03cfa8b4fb26e82956e19285d4b2f7d6c9ab23de
Stephanie Jones <stephanie.nicole.jones78@gmail.com>
2021-12-21 10:31:05 -0600

    through explore

--- wrangle.py
+++ wrangle.py
@@ -1,39 +1,15 @@
-######################################################################## FILE USED TO ACQUIRE AND WRANGLE ZILLOW
-
-###############################################################| IMPORTS
-import os
-
-#stored db credentials (use ignore file to hide)
-import env
-
-#python libraries
 import numpy as np
 import pandas as pd
-
-#visualization libraries
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#sklearn library
+import env
+
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
-
-###############################################################| CONNECTION FUNCTION
-def get_connection(db):
-    '''
-THIS FUNCTION TAKES IN A DB FROM SQL SERVER AND USES THE SERVER CREDENTIALS FROM ENV
-FILE TO RETURN A STRING THAT WILL BE USED TO CONNECT TO THE SQL SERVER. 
-    '''
-    #server access credentials
-#    from env import host, user, password
-    
-    #url connection string
-    return f'mysql+pymysql://{env.user}:{env.password}@{env.host}/{db}'
-
-
-###############################################################| SQL QUERY
-qry = '''
+sql = """
 SELECT prop.*, 
        pred.logerror, 
        pred.transactiondate, 
@@ -60,35 +36,131 @@ FROM   properties_2017 prop
        LEFT JOIN storytype story USING (storytypeid) 
        LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid) 
 WHERE  prop.latitude IS NOT NULL 
-       AND prop.longitude IS NOT NULL AND transactiondate <= '2017-12-31'; 
-            '''
+       AND prop.longitude IS NOT NULL AND transactiondate <= '2017-12-31' 
+"""
+
+def get_db_url(database):
+    from env import host, user, password
+    url = f'mysql+pymysql://{user}:{password}@{host}/{database}'
+    return url
 
 
-###############################################################| ACQUIRE DATA FUNCTION
-def acquire_data(file_name, db):
-    '''
-THIS FUNCTION TAKES IN A CSV FILE NAME AND A DB FROM THE SQL SERVER. IF THE FILE EXISTS
-IN THE LOCAL DIRECTORY IT WILL WRITE IT TO A DF. IF IT DOES NOT IT WILL USE THE DB AND 
-CONNECTION FUNCTION TO WRITE THE SQL QUERY TO A DB AND THEN WRITE THE QUERY TO A CSV TO
-BE SAVED LOCALLY.
-    '''
+# acquire zillow data using the query
+def get_zillow(sql):
+    url = get_db_url('zillow')
+    zillow_df = pd.read_sql(sql, url, index_col='id')
+    return zillow_df
+
+
+def handle_missing_values(df, prop_required_column = .5, prop_required_row = .70):
+	#function that will drop rows or columns based on the percent of values that are missing:\
+	#handle_missing_values(df, prop_required_column, prop_required_row
+    threshold = int(round(prop_required_column*len(df.index),0))
+    df.dropna(axis=1, thresh=threshold, inplace=True)
+    threshold = int(round(prop_required_row*len(df.columns),0))
+    df.dropna(axis=0, thresh=threshold, inplace=True)
+    return df
+
+
+def remove_columns(df, cols_to_remove):  
+	#remove columns not needed
+    df = df.drop(columns=cols_to_remove)
+    return df
+
+def wrangle_zillow():
+    df = pd.read_csv('zillow.csv')
     
-    # if csv data file already exists in local directory...
-    if os.path.isfile(file_name):
-        
-        # write the data to dataframe
-        df = pd.read_csv(file_name)
+    # Restrict df to only properties that meet single unit use criteria
+    single_use = [261, 262, 263, 264, 266, 268, 273, 276, 279]
+    df = df[df.propertylandusetypeid.isin(single_use)]
     
-    # if csv data file does not exist in local direction ...
-    else:
-        
-        #url string from preceding function, uses to establish server connection
-        url = get_connection(db)
+    # Restrict df to only those properties with at least 1 bath & bed and 350 sqft area
+    df = df[(df.bedroomcnt > 0) & (df.bathroomcnt > 0) & ((df.unitcnt<=1)|df.unitcnt.isnull())\
+            & (df.calculatedfinishedsquarefeet>350)]
 
-        #writing sql query to df
-        df = pd.read_sql(qry, url)
-        
-        #saving data as a csv file locally
-        df.to_csv(file_name)
+    # Handle missing values i.e. drop columns and rows based on a threshold
+    df = handle_missing_values(df)
+    
+    # Add column for counties
+    df['county'] = np.where(df.fips == 6037, 'Los_Angeles',
+                           np.where(df.fips == 6059, 'Orange', 
+                                   'Ventura'))    
+    # drop columns not needed
+    df = remove_columns(df, ['id',
+       'calculatedbathnbr', 'finishedsquarefeet12', 'fullbathcnt', 'heatingorsystemtypeid'
+       ,'propertycountylandusecode', 'propertylandusetypeid','propertyzoningdesc', 
+        'censustractandblock', 'propertylandusedesc','heatingorsystemdesc','unitcnt'
+                            ,'buildingqualitytypeid'])
+    
+    # replace nulls with median values for select columns
+    df.lotsizesquarefeet.fillna(7313, inplace = True)
+
+    # Columns to look for outliers
+    df = df[df.taxvaluedollarcnt < 5_000_000]
+    df[df.calculatedfinishedsquarefeet < 8000]
+    
+    # Just to be sure we caught all nulls, drop them here
+    df = df.dropna()
     
     return df
+
+def min_max_scaler(train, valid, test):
+    '''
+    Uses the train & test datasets created by the split_my_data function
+    Returns 3 items: mm_scaler, train_scaled_mm, test_scaled_mm
+    This is a linear transformation. Values will lie between 0 and 1
+    '''
+    # SAVING ALL OF THE NUMBER DTYPE COLUMNS INTO A LIST
+    num_vars = list(train.select_dtypes('number').columns)
+    
+    # CREATING SCALER OBJECT
+    scaler = MinMaxScaler(copy=True, feature_range=(0,1))
+    
+    # FITTING THE SCALER OBJECT TO TRAIN AND TRANSFORMING
+    train[num_vars] = scaler.fit_transform(train[num_vars])
+    
+    # TRANSFORMING VALIDATE AND TEST DATA USING SCALER OBJECT THAT WAS FITTED ON TRAIN
+    valid[num_vars] = scaler.transform(valid[num_vars])
+    test[num_vars] = scaler.transform(test[num_vars])
+    
+    return scaler, train, valid, test
+
+def outlier_function(df, cols, k):
+	#function to detect and handle oulier using IQR rule
+    for col in df[cols]:
+        q1 = df.annual_income.quantile(0.25)
+        q3 = df.annual_income.quantile(0.75)
+        iqr = q3 - q1
+        upper_bound =  q3 + k * iqr
+        lower_bound =  q1 - k * iqr     
+        df = df[(df[col] < upper_bound) & (df[col] > lower_bound)]
+    return df
+
+def get_mall_customers(sql):
+	    url = get_db_url('mall_customers')
+	    mall_df = pd.read_sql(sql, url, index_col='customer_id')
+	    return mall_df
+
+def wrangle_mall_df():
+    
+    # acquire data
+    sql = 'select * from customers'
+
+
+    # acquire data from SQL server
+    mall_df = get_mall_customers(sql)
+    
+    # handle outliers
+    mall_df = outlier_function(mall_df, ['age', 'spending_score', 'annual_income'], 1.5)
+    
+    # get dummy for gender column
+    dummy_df = pd.get_dummies(mall_df.gender, drop_first=True)
+    mall_df = pd.concat([mall_df, dummy_df], axis=1).drop(columns = ['gender'])
+    mall_df.rename(columns= {'Male': 'is_male'}, inplace = True)
+    # return mall_df
+
+    # split the data in train, validate and test
+    train, test = train_test_split(mall_df, train_size = 0.8, random_state = 123)
+    train, validate = train_test_split(train, train_size = 0.75, random_state = 123)
+    
+    return min_max_scaler, train, validate, test
