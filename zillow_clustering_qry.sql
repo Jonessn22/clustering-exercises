9fe7867e39e2adcf22ba4db498a7198d0acf80fe
Stephanie Jones <stephanie.nicole.jones78@gmail.com>
2021-12-29 13:56:09 -0600

    notes for 01 wrangle, creating master wrangle.py module, and finishing exercises

--- zillow_clustering_qry.sql
+++ zillow_clustering_qry.sql
@@ -1,27 +1,42 @@
-SELECT prop.*, 
-       pred.logerror, 
-       pred.transactiondate, 
-       air.airconditioningdesc, 
-       arch.architecturalstyledesc, 
-       build.buildingclassdesc, 
-       heat.heatingorsystemdesc, 
-       landuse.propertylandusedesc, 
-       story.storydesc, 
-       construct.typeconstructiondesc 
+/*
+SQL PRACTICE, ARTICLE "How to Use the SQL GROUP BY Clause and Aggregate Functions" BY TERENCE SHIN 
+(https://towardsdatascience.com/how-to-use-the-sql-group-by-clause-and-aggregate-functions-cbaef410be91)
+*/
 
-FROM   properties_2017 prop  
-       INNER JOIN (SELECT parcelid,
-       					  logerror,
-                          Max(transactiondate) transactiondate 
-                   FROM   predictions_2017 
-                   GROUP  BY parcelid, logerror) pred
-               USING (parcelid) 
-       LEFT JOIN airconditioningtype air USING (airconditioningtypeid) 
-       LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid) 
-       LEFT JOIN buildingclasstype build USING (buildingclasstypeid) 
-       LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid) 
-       LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid) 
-       LEFT JOIN storytype story USING (storytypeid) 
-       LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid) 
-WHERE  prop.latitude IS NOT NULL 
-       AND prop.longitude IS NOT NULL AND transactiondate <= '2017-12-31';                  
+/*displaying the whole table*/
+SELECT * 
+FROM albums;
+
+/*showing the total count of artists listed, and the number of unique artist names*/
+SELECT COUNT(DISTINCT artist) AS 'unique artists',
+	COUNT(artist) AS 'total artists'
+FROM albums;
+
+/*trying to show count for albumbs with 1980 release date; ERROR: "execute command denied to user 'hopper_1546'@'%' for routine 'albums_db.COUNTIF'"*/
+SELECT COUNTIF(release_date = 1980)  AS count
+FROM albums;
+
+/*taking the sum of total album sales*/
+SELECT SUM(sales)
+FROM albums;
+
+/*total sales by genre*/
+SELECT genre, SUM(sales) AS sales_by_genre
+FROM albums
+GROUP BY genre;
+
+/*avergae sales by genre*/
+SELECT genre, AVG(sales) AS avg_sales_by_genre
+FROM albums
+GROUP BY genre;
+
+/*average release date and sales*/
+SELECT AVG(release_date) AS avg_release_date,
+	AVG(sales) AS avg_sales
+FROM albums;
+
+/*average sales by release year*/
+SELECT release_date, AVG(sales) AS sales_by_release_date
+FROM albums
+GROUP BY release_date;
+
